af2148e10797536ac19d1ce6900d7f472d31f010
Kauan Vieira <kauan.ssv0@gmail.com>
2024-10-27 17:17:32 -0300

    add new 3 files

---
+++ js/aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+if(vel > 60){
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO`)
+}else{
+    console.log(`Está na velocidade limite`)
+}
---
+++ js/aula011/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+console.log(`Você nasceu em: ${pais}`)
+if(pais == 'brasil'){
+    console.log('Você é: Brasileiro')
+}else{
+    console.log('Você é: Estrangeiro')
+}
