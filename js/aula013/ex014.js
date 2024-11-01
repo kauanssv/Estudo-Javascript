580e8c7d0d922cfd3711c5001597c7ccae8cf4e5
Kauan Vieira <kauan.ssv0@gmail.com>
2024-11-01 15:58:17 -0300

    while, do while, for

---
+++ js/aula013/ex014.js
@@ -0,0 +1,11 @@
+var a = 1
+var b = 1
+while(a <= 3){
+    console.log(`Passo ${a} com while`)
+    a++
+}
+console.log('---------')
+do{
+    console.log(`Passo ${b} com do while`)
+    b++
+}while(b <= 3)
